b514c27f1e80be083e99e5c2d9ab774e48538fd4
zhoumiqi <354642263@qq.com>
2016-12-08 14:57:21 +0800

    测试commit and push

--- app/src/main/java/com/demo/helloworld/MainActivity.java
+++ app/src/main/java/com/demo/helloworld/MainActivity.java
@@ -2,12 +2,33 @@ package com.demo.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        findViewById(R.id.btn_show_toast).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_show_toast:
+                Toast.makeText(this,"测试Toast",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
     }
 }
